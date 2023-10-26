c89fdafcd389365f93e6378ba4e8d5e8e88a42e8
azrizzz <lenovorosak@gmail.com>
2023-10-26 23:46:05 +0800

    fix: update og image

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ import { SiteConfig } from "./../types";
 export const siteConfig: SiteConfig = {
   name: "PayLater Insight",
   description:
-    "PayLater-Insight: Instantly calculate and analyze interest on your SPayLater plans, empowering smarter purchase decisions.",
+    "Instantly calculate and analyze interest on your SPayLater plans, empowering smarter purchase decisions.",
   url: "https://www.paylaterinsight.com",
   ogImage: "https://www.paylaterinsight.com/paylater-insight.png",
   links: {
